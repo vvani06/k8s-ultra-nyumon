d87afd369e6caa71e6f4a5b7c0491f22f91ac91d
vvani <vvani06@gmail.com>
2020-02-25 03:17:28 +0900

    スライドを書く過程での改善ポイントを適用

--- api/src/main.cr
+++ api/src/main.cr
@@ -5,14 +5,10 @@ REDIS_HOST = ENV["REDIS_HOST"]
 REDIS_PORT = ENV["REDIS_PORT"].to_i
 
 redis = Redis.new(host: REDIS_HOST, port: REDIS_PORT)
-redis.set("count", 0)
-redis.close
+at_exit { redis.close }
 
 get "/" do
-  redis = Redis.new(host: REDIS_HOST, port: REDIS_PORT)
   count = redis.incr("count")
-  redis.close
-
   "Hello World! ##{count}"
 end
 
